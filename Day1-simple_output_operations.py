e7f8d701e157dcac8b3b2973bf2b5d4b65de4766
prem sai <premsaips1619@gmail.com>
2026-01-07 22:17:38 +0530

    Add files via upload

---
+++ Day1-simple_output_operations.py
@@ -0,0 +1,3 @@
+print("My self Prem sai")
+print("Day 1 Python at codegnan")
+print("i am performing a basic output operations")
---
+++ Day2-Variables_Datatypes_input,output_functions.py
@@ -0,0 +1,21 @@
+#Data Types, Variables and Input / Output Functions
+Name = "Prem Sai Potham setty"  #string
+Age = 38   #Interger value
+Btech_cgpa = 8.5  #Decimal value
+Is_passed = True  # Boolean value
+list1 = ["hi", 1, 2.3, True, (1,3)]  #List of items
+tuple1 = (10, 20, 30, "welcome", [1,2])  #Tuple data type
+cards = {"K", "Q", "J", 1, 2, 3,7, 8, 9, 3, 5, 7, 4, 5, 6}  #Set data type
+marks = {"Tel" : 50, "Hin" : 20, "Maths" : 100}  #Dictionary data type
+
+print(f"My name is : {Name}")    
+print("I am ", Age, "years old.")
+print("My Btech cgpa is :", Btech_cgpa)
+print("Am i passed or not :", Is_passed)
+print("List of items : ", list1)
+print("Deck of cards : ", cards)
+print("Subject wise marks : ", marks)
+
+
+
+
